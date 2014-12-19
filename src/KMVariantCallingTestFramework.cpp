b8084c416c648a8cc8f069c0d2d4e41e3c142e98
Amir <amir.n3t@gmail.com>
2014-12-19 13:49:03 +1100

    update

--- src/KMVariantCallingTestFramework.cpp
+++ src/KMVariantCallingTestFramework.cpp
@@ -7,13 +7,64 @@
 //============================================================================
 
 #include <iostream>
+#include "string.h"
 #include "VCFModel.h"
 using namespace std;
+void ReadKeys(int argc, char* argv[])
+{
+	string FileAddress="";
+	int TotalReads=0;
+	int TotalVariants=0;
+
+	 if (argc < 3) { // Check the value of argc. If not enough parameters have been passed, inform user and exit.
+	        std::cout << "Usage is -vcf <refrence>  -t <total reads> -v<total variants>"<<endl<<endl; // Inform the user of how to use the program
+	    } else { // if we got enough parameters...
+	        char* myFile, myPath, myOutPath;
+	        std::cout << argv[0];
+	        for (int i = 1; i < argc; i+=2) { /* We will iterate over argv[] to get the parameters stored inside.
+	                                          * Note that we're starting on 1 because we don't need to know the
+	                                          * path of the program, which is stored in argv[0] */
+	            if (i + 1 != argc) // Check that we haven't finished parsing already
+	                if (strcmp(argv[i] ,"-vcf")==0) {
+	                    // We know the next argument *should* be the filename:
+	                    FileAddress = argv[i + 1];
+	                }
+	                else if (strcmp(argv[i] , "-t")==0) {
+	                    TotalReads = stoi( argv[i + 1]);
+	                }
+	                else if (strcmp(argv[i], "-v")==0) {
+	                    TotalVariants = stoi(argv[i + 1]);
+	                }
+	                else {
+	                    std::cout << "Not enough or invalid arguments, please try again."<<endl;
+	                    std::cout <<"PARAMETERS:"<< argv[i] << " "<<endl;
+	            }
+
+	        }
+	    }
+	 if (FileAddress.length()==0)
+	 {
+		 cout <<"Reference address is not specified."<<endl;
+		 return;
+	 }
+	 else if (TotalReads<1)
+	 {
+		 cout <<"Reads should be more than 0";
+		 return;
+	 }
+	 else if (TotalVariants<0)
+	 {
+		 cout <<"Variants should be 0+";
+		 return;
+	 }
+
+	 //cout <<"Preparing with Ref:"<<RefAdr<<endl<<"Output"<<Output<<endl;
+	 CheckSingleVariantRatio(TotalVariants,TotalReads,FileAddress);
+}
 
 int main(int argc, char* argv[]) {
 	cout <<endl<< "Random Read Generator initializing" << endl;
 	//ReadKeys(argc,argv);
-	//GetVCFContent();
-	CheckSingleVariantRatio(45,95,"/Users/Amir/Desktop/GATK/EXP/GATK\ Testing\ Scripts/2-SNPCall/raw_variants.vcf");
+
 	return 0;
 }
--- src/README.md
+++ src/README.md
@@ -1,47 +1,5 @@
-KMReadGenerator
+KMVariantCallingTestFramework
 ===============
-This command line tool helps generating reads that may have overlap or not.
+This command line tool helps testing different Variant calling programs
 
-HOW TO USE ?
-
-./KMReadGenerator -r "chrM.txt" -o "output.fa"
-
--r: reference address. (required)
-	reference file should be in a FASTA format.
-
--o: output file address(required)
-
--l: Length of generated reads.[default =100] (optional)
-	length should be 10+ 
-
--v: Variant type [default =none] (optional)
-	s=substitution , i=insertion , d=deletion, n=none;
-
--vp:Variant percentage [default =50] (optional)
-	
--n: number of generated reads[default =10]  (optional)
-	number should be 1+
-	
-	
-Sample output :
-
->Read1_0-50
-TTGGTATTTTCGTCTGGGGGGTGTGCACGCGATAGCATTGCGAGACGCTG
->Read2_5-100
-ATTTTCGTCTGGGGGGTGTGCACGCGATAGCATTGCGAGACGCTGGAGCC
->Read3_10-150
-CGTCTGGGGGGTGTGCACGCGATAGCATTGCGAGACGCTGGAGCCGGAGC
->Read4_15-200
-GGGGGGTGTGCACGCGATAGCATTGCGAGACGCTGGAGCCGGAGCACCCT
->Read5_20-250
-GTGTGCACGCGATAGCATTGCGAGACGCTGGAGCCGGAGCACCCTATGTC
->Read6_25-300
-CACGCGATAGCATTGCGAGACGCTGGAGCCGGAGCACCCTATGTCGCAGT
->Read7_30-350
-GATAGCATTGCGAGACGCTGGAGCCGGAGCACCCTATGTCGCAGTATCTG
->Read8_35-400
-CATTGCGAGACGCTGGAGCCGGAGCACCCTATGTCGCAGTATCTGTCTTT
->Read9_40-450
-CGAGACGCTGGAGCCGGAGCACCCTATGTCGCAGTATCTGTCTTTGATTC
->Read10_45-500
-CGCTGGAGCCGGAGCACCCTATGTCGCAGTATCTGTCTTTGATTCCTGCC
+./KMVariantCallingTestFramework -vcf "/Users/Amir/Desktop/GATK/EXP/GATK\ Testing\ Scripts/2-SNPCall/raw_variants.vcf" -r 95 -v 45
