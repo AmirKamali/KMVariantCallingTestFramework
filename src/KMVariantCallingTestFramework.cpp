eb273b8919d2eb3faad616d3322eb93034f14975
Amir <amir.n3t@gmail.com>
2014-12-19 13:34:13 +1100

    s

---
+++ src/KMVariantCallingTestFramework.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : KMVariantCallingTestFramework.cpp
+// Author      : AmirHossein Kamali
+// Version     :
+// Copyright   : Victor Chang Cardiac Institute 2014
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "HII" << endl; // prints HII
+	return 0;
+}
